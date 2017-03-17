dc5c098897684103e132d7ddffd17ae21a553938
jackychenb <jackychenb@jackychenbtekiMacBook-Pro.local>
2017-03-17 11:48:46 +0800

    Remove SearchService

--- omega-framework-index-common/src/main/java/com/omega/framework/index/IndexCommandService.java
+++ omega-framework-index-common/src/main/java/com/omega/framework/index/IndexCommandService.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.omega.framework.index.bean.GUID;
 import com.omega.framework.index.bean.IndexCommand;
 import org.apache.commons.lang.StringUtils;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.elasticsearch.client.transport.TransportClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -19,7 +22,7 @@ import java.util.concurrent.TimeUnit;
  * Created by jackychenb on 11/12/2016.
  */
 
-public class IndexService {
+public class IndexCommandService {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -41,6 +44,15 @@ public class IndexService {
 
     @Value("${elasticsearch.refresh.duration:500}")
     private long refreshDuration = 500L; // 预估的最长的refresh调用耗时，单位毫秒
+
+    @Autowired
+    private CuratorFramework curatorFramework;
+
+    @Autowired
+    private TransportClient elasticsearchClient;
+
+    @Value("${elasticsearch.refresh.lockPath:/index/refresh}")
+    private String lockPath = "/index/refresh";
 
     public String exec(final IndexCommand cmd) {
         checkCommand(cmd);
@@ -105,6 +117,47 @@ public class IndexService {
         int op = cmd.getOp();
         if (IndexCommand.OP_ADD != op && IndexCommand.OP_DELETE != op) {
             throw new IllegalArgumentException("Illegal index operation: " + op);
+        }
+    }
+
+    private String getCacheKey(String indexName) {
+        return indexName;
+    }
+
+    private String getLockName(String indexName) {
+        return lockPath + "/" + indexName;
+    }
+
+    public void ensureRefresh(String indexName, String lastCommandId) throws Exception {
+        Long submitTime = (Long) redisTemplate.opsForValue().get(lastCommandId);
+        if (submitTime == null) {
+            return;
+        }
+
+        String key = getCacheKey(indexName);
+        Long refreshTime = (Long) redisTemplate.opsForValue().get(key);
+        if (refreshTime != null && submitTime.compareTo(refreshTime) < 0) {
+            return;
+        }
+
+        String lockName = getLockName(indexName);
+        InterProcessMutex lock = new InterProcessMutex(curatorFramework, lockName);
+
+        try {
+            if (lock.acquire(refreshDuration, TimeUnit.MILLISECONDS)) {
+                // 获得锁后再次检查索引库更新时间
+                Long refreshTime2 = (Long) redisTemplate.opsForValue().get(key);
+                if (refreshTime2 != null && submitTime.compareTo(refreshTime2) < 0) {
+                    return;
+                }
+
+                // 时间戳一定要在调用ES刷新接口前获得
+                long now = System.currentTimeMillis();
+                elasticsearchClient.admin().indices().prepareRefresh(indexName).get();
+                redisTemplate.opsForValue().set(key, now);
+            }
+        } finally {
+            lock.release();
         }
     }
 
